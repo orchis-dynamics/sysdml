b0c95095f1449320633246065ebc85ffabc50fa3
vesely-petr <veselypetr@mail.com>
2026-05-28 16:17:59 +0200

    renderer: install Trusted Types policy at app boot

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { installTrustedTypesPolicy } from "./security/trusted-types.js";
 import "./style.css";
 
+installTrustedTypesPolicy();
 createApp(App).mount("#app");
