ffce5530ee8bcf7dc5dde6e1977c1f305f67d584
vesely-petr <veselypetr@mail.com>
2026-05-31 19:33:59 +0200

    Add vueuse dependency to renderer; remove dead code

--- packages/renderer/src/transport/index.ts
+++ packages/renderer/src/transport/index.ts
@@ -1,21 +1,17 @@
-import type { IRTransport } from "./types.js";
 import { NullAdapter } from "./null.js";
 import { PostMessageAdapter } from "./postmessage.js";
+import type { IRTransport } from "./types.js";
 import { WebSocketAdapter } from "./websocket.js";
 
-declare const window: Window & {
-  SYSDML_WS_URL?: string;
-};
-
 export function createTransport(): IRTransport {
-  if (typeof acquireVsCodeApi !== "undefined") {
-    return new PostMessageAdapter();
-  }
+	if (typeof acquireVsCodeApi !== "undefined") {
+		return new PostMessageAdapter();
+	}
 
-  const wsUrl = WebSocketAdapter.resolveUrl();
-  if (wsUrl !== null) {
-    return new WebSocketAdapter(wsUrl);
-  }
+	const wsUrl = WebSocketAdapter.resolveUrl();
+	if (wsUrl !== null) {
+		return new WebSocketAdapter(wsUrl);
+	}
 
-  return new NullAdapter();
+	return new NullAdapter();
 }
