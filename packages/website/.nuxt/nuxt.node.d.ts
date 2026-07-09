f0a79d8e471551a690961fbeaab5ba1bee4b0b6a
peter-btw <71469387+peter-btw@users.noreply.github.com>
2026-07-09 21:50:18 +0200

    remove website from this repo (#58)

---
+++ packages/website/.nuxt/nuxt.node.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="@nuxt/telemetry" />
+/// <reference types="@nuxt/devtools" />
+/// <reference path="types/nitro-layouts.d.ts" />
+/// <reference path="types/modules.d.ts" />
+/// <reference path="types/runtime-config.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference types="nuxt" />
+/// <reference path="../../../node_modules/.pnpm/@nuxt+vite-builder@4.4.8_@babel+plugin-syntax-jsx@7.29.7_@babel+core@7.29.7__@types+node@24.1_vsps5xb5xx5ltiff2ho4uqs4hm/node_modules/@nuxt/vite-builder/dist/index.d.mts" />
+/// <reference path="../../../node_modules/.pnpm/@nuxt+nitro-server@4.4.8_@azure+identity@4.13.1_@babel+plugin-syntax-typescript@7.29.7_@babel_whs3mk2q5s2cmkvc5ihanmdk5e/node_modules/@nuxt/nitro-server/dist/index.d.mts" />
+/// <reference path="types/middleware.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+
+export {}
